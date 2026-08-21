7f88d3ca732fe90af5a6cd715c479eb89b789488
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-08-21 10:50:47 +1000

    chore: migrate to platform-cdk-constructs 1.7.2 and use GitStack
    - Bump @orcabus/platform-cdk-constructs from 1.5.4 to 1.7.2
    - Update StatefulApplicationStack to extend GitStack
    - Update StatelessApplicationStack to extend GitStack
    
    GitStack adds a GitCommitId CloudFormation output to each deployed
    stack for deployment traceability.

--- infrastructure/stage/stateful-application-stack.ts
+++ infrastructure/stage/stateful-application-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { GitStack } from '@orcabus/platform-cdk-constructs/deployment-stack-pipeline';
 import { StatefulApplicationStackConfig } from './interfaces';
 import { buildSsmParameters } from './ssm';
 
 export type StatefulApplicationStackProps = StatefulApplicationStackConfig & cdk.StackProps;
 
-export class StatefulApplicationStack extends cdk.Stack {
+export class StatefulApplicationStack extends GitStack {
   constructor(scope: Construct, id: string, props: StatefulApplicationStackProps) {
     super(scope, id, props);
 
